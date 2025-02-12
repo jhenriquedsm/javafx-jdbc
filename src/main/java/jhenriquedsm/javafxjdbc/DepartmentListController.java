8dcd97228044423bdbcb8f2511e43bbcd3db408f
jhenriquedsm <jhsilvamata@gmail.com>
2025-02-12 15:59:16 -0300

    DepartmentForm design - dialog

--- src/main/java/jhenriquedsm/javafxjdbc/DepartmentListController.java
+++ src/main/java/jhenriquedsm/javafxjdbc/DepartmentListController.java
@@ -2,16 +2,25 @@ package jhenriquedsm.javafxjdbc;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import jhenriquedsm.javafxjdbc.model.entities.Department;
 import jhenriquedsm.javafxjdbc.model.services.DepartmentService;
+import jhenriquedsm.javafxjdbc.util.Alerts;
+import jhenriquedsm.javafxjdbc.util.Utils;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -35,8 +44,9 @@ public class DepartmentListController implements Initializable {
     private ObservableList<Department> observableList;
 
     @FXML
-    public void onBtNewAction() {
-        System.out.println("onBtNewAction");
+    public void onBtNewAction(ActionEvent actionEvent) {
+        Stage parentStage = Utils.currentStage(actionEvent);
+        createDialogForm("DepartmentForm.fxml", parentStage);
     }
 
     public void setDepartmentService(DepartmentService departmentService) {
@@ -63,5 +73,22 @@ public class DepartmentListController implements Initializable {
         List<Department> departments = departmentService.findAll();
         observableList = FXCollections.observableArrayList(departments);
         tableViewDepartment.setItems(observableList);
+    }
+
+    private void createDialogForm(String absoluteName, Stage parentStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            Pane pane = loader.load();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Enter Department data");
+            dialogStage.setScene(new Scene(pane));
+            dialogStage.setResizable(false);
+            dialogStage.initOwner(parentStage);
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.showAndWait();
+        } catch (IOException e) {
+            Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
---
+++ src/main/java/jhenriquedsm/javafxjdbc/util/Utils.java
@@ -0,0 +1,11 @@
+package jhenriquedsm.javafxjdbc.util;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
+public class Utils {
+    public static Stage currentStage(ActionEvent actionEvent) {
+        return (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+    }
+}
