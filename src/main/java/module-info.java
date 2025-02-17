14b3f2f025001274ff97e759839199535e189da6
jhenriquedsm <jhsilvamata@gmail.com>
2025-02-17 11:03:59 -0300

    SellerTableView

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module jhenriquedsm.javafxjdbc {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires jdk.jshell;
 
     opens jhenriquedsm.javafxjdbc.model.entities to javafx.graphics, javafx.fxml, javafx.base;
     opens jhenriquedsm.javafxjdbc to javafx.fxml;
